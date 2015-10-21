b69945797d1163163c1c698a59cabd028dba7110
sarakha63 <sarakha_ludovic@yahoo.fr>
2015-10-21 20:17:29 +0200

    Update netatmoWeather.class.php

--- core/class/netatmoWeather.class.php
+++ core/class/netatmoWeather.class.php
@@ -174,7 +174,7 @@ class netatmoWeather extends eqLogic {
 						$battery_min = 3950;
 					}
 					if ($battery_max != null && $battery_min != null) {
-						$battery = ($module['battery_vp'] - $battery_min) / ($battery_max-$$battery_min);
+						$battery = ($module['battery_vp'] - $battery_min) / ($battery_max-$battery_min);
 					}
 					if ($battery < 0) {
 						$battery = 0;
