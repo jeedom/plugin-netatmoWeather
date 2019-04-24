6ad2fe3f9398b49d5437eeee5251e4674bf58996
Loïc <loic@jeedom.com>
2019-04-24 15:47:41 +0200

    Update netatmoWeather.php

--- desktop/php/netatmoWeather.php
+++ desktop/php/netatmoWeather.php
@@ -66,7 +66,6 @@ foreach ($eqLogics as $eqLogic) {
   <div class="col-sm-6">
     <form class="form-horizontal">
      <fieldset>
-      <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
       <div class="form-group">
         <label class="col-sm-4 control-label">{{Nom de l'équipement météo Netatmo}}</label>
         <div class="col-sm-6">
