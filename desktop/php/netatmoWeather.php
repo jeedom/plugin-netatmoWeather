5756e817ae87409b2802b0bc14855a584a53524a
Loïc <loic@jeedom.com>
2019-06-28 19:01:23 +0200

    improve ui

--- desktop/php/netatmoWeather.php
+++ desktop/php/netatmoWeather.php
@@ -8,48 +8,48 @@ $eqLogics = eqLogic::byType($plugin->getId());
 ?>
 
 <div class="row row-overflow">
- <div class="col-xs-12 eqLogicThumbnailDisplay">
-   <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
-   <div class="eqLogicThumbnailContainer">
-  <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
-      <i class="fa fa-wrench"></i>
-	  	<br>
-    <span>{{Configuration}}</span>
-  </div>
-  <div class="cursor logoSecondary" id="bt_healthNetatmoWeather">
-      <i class="fa fa-medkit"></i>
-    	<br>
-    <span>{{Santé}}</span>
-  </div>
-</div>
-  <legend><i class="icon nature-weather1"></i>  {{Mes Stations}}
-  </legend>
-  <?php
-if (count($eqLogics) == 0) {
-	echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Vous n'avez pas encore de station Netatmo, aller sur Général -> Plugin et cliquez sur synchroniser pour commencer}}</span></center>";
-} else {
-	?>
-   <div class="eqLogicThumbnailContainer">
-    <?php
-foreach ($eqLogics as $eqLogic) {
-		$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
-		echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
-		if ($eqLogic->getConfiguration('type', '') != '') {
-			echo '<img src="plugins/netatmoWeather/core/img/' . $eqLogic->getConfiguration('type', '') . '.png" />';
+	<div class="col-xs-12 eqLogicThumbnailDisplay">
+		<legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+		<div class="eqLogicThumbnailContainer">
+			<div class="cursor eqLogicAction logoPrimary" data-action="gotoPluginConf">
+				<i class="fa fa-wrench"></i>
+				<br>
+				<span>{{Configuration}}</span>
+			</div>
+			<div class="cursor logoSecondary" id="bt_healthNetatmoWeather">
+				<i class="fa fa-medkit"></i>
+				<br>
+				<span>{{Santé}}</span>
+			</div>
+		</div>
+		<legend><i class="icon nature-weather1"></i> {{Mes Stations}}</legend>
+		<input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
+		<?php
+		if (count($eqLogics) == 0) {
+			echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Vous n'avez pas encore de station Netatmo, aller sur Général -> Plugin et cliquez sur synchroniser pour commencer}}</span></center>";
 		} else {
-			echo '<img src="' . $plugin->getPathImgIcon() . '" />';
-		}
-		echo '<br/>';
-		echo '<span>' . $eqLogic->getHumanName(true, true) . '</span>';
-		echo '</div>';
-	}
-	?>
- </div>
- <?php }
-?>
-</div>
-<div class="col-xs-12 eqLogic" style="display: none;">
-	<div class="input-group pull-right" style="display:inline-flex">
+			?>
+			<div class="eqLogicThumbnailContainer">
+				<?php
+				foreach ($eqLogics as $eqLogic) {
+					$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+					echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+					if ($eqLogic->getConfiguration('type', '') != '') {
+						echo '<img src="plugins/netatmoWeather/core/img/' . $eqLogic->getConfiguration('type', '') . '.png" />';
+					} else {
+						echo '<img src="' . $plugin->getPathImgIcon() . '" />';
+					}
+					echo '<br/>';
+					echo '<span>' . $eqLogic->getHumanName(true, true) . '</span>';
+					echo '</div>';
+				}
+				?>
+			</div>
+		<?php }
+		?>
+	</div>
+	<div class="col-xs-12 eqLogic" style="display: none;">
+		<div class="input-group pull-right" style="display:inline-flex">
 			<span class="input-group-btn">
 				<a class="btn btn-default btn-sm eqLogicAction roundedLeft" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="copy"><i class="fas fa-copy"></i> {{Dupliquer}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
 			</span>
@@ -62,102 +62,102 @@ foreach ($eqLogics as $eqLogic) {
 		
 		<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
 			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
- <div class="row">
-  <div class="col-sm-6">
-    <form class="form-horizontal">
-     <fieldset>
-      <div class="form-group">
-        <label class="col-sm-4 control-label">{{Nom de l'équipement météo Netatmo}}</label>
-        <div class="col-sm-6">
-          <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-          <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement météo Netatmo}}"/>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-4 control-label" >{{Objet parent}}</label>
-        <div class="col-sm-6">
-          <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-            <option value="">{{Aucun}}</option>
-            <?php
-foreach (object::all() as $object) {
-	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-}
-?>
-         </select>
-       </div>
-     </div>
-     <div class="form-group">
-       <label class="col-sm-4 control-label"></label>
-       <div class="col-sm-8">
-	<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-        <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-       </div>
-     </div>
-     <div class="form-group">
-      <label class="col-sm-4 control-label">{{Identifiant}}</label>
-      <div class="col-sm-6">
-        <span class="eqLogicAttr label label-info" style="font-size:1em;" data-l1key="logicalId"></span>
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="col-sm-4 control-label">{{Type}}</label>
-      <div class="col-sm-6">
-        <select type="text" disabled class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="type" >
-          <option value="station">{{Station}}</option>
-          <option value="module_ext">{{Module extérieur}}</option>
-          <option value="module_int">{{Module intérieur}}</option>
-          <option value="module_rain">{{Module pluie}}</option>
-          <option value="module_wind">{{Anémomètre}}</option>
-        </select>
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="col-sm-4 control-label">{{Firmware}}</label>
-      <div class="col-sm-6">
-        <span class="eqLogicAttr label label-info" style="font-size:1em;" data-l1key="configuration" data-l2key="firmware"></span>
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="col-sm-4 control-label">{{Réception réseaux}}</label>
-      <div class="col-sm-6">
-        <span class="label label-info" style="font-size:1em;">
-          <span class="eqLogicAttr" data-l1key="configuration" data-l2key="wifi_status"></span>
-          <span class="eqLogicAttr" data-l1key="configuration" data-l2key="rf_status"></span>
-        </span>
-      </div>
-    </div>
-    <div class="form-group" id="battery_net_weather">
-      <label class="col-sm-4 control-label">{{Batterie}}</label>
-      <div class="col-sm-6">
-        <span class="label label-info" style="font-size:1em;">
-          <span class="eqLogicAttr" data-l1key="configuration" data-l2key="batteryStatus"></span> %
-        </span>
-      </div>
-    </div>
-  </fieldset>
-</form>
-</div>
-<div class="col-sm-6">
-  <center>
-    <img src="' . $plugin->getPathImgIcon() . '" id="img_netatmoModel" style="height : 300px;margin-top : 60px" />
-  </center>
-</div>
-</div>
-	</div>
+				<div class="row">
+					<div class="col-sm-6">
+						<form class="form-horizontal">
+							<fieldset>
+								<div class="form-group">
+									<label class="col-sm-4 control-label">{{Nom de l'équipement météo Netatmo}}</label>
+									<div class="col-sm-6">
+										<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+										<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement météo Netatmo}}"/>
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-sm-4 control-label" >{{Objet parent}}</label>
+									<div class="col-sm-6">
+										<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+											<option value="">{{Aucun}}</option>
+											<?php
+											foreach (object::all() as $object) {
+												echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+											}
+											?>
+										</select>
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-sm-4 control-label"></label>
+									<div class="col-sm-8">
+										<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+										<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-sm-4 control-label">{{Identifiant}}</label>
+									<div class="col-sm-6">
+										<span class="eqLogicAttr label label-info" style="font-size:1em;" data-l1key="logicalId"></span>
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-sm-4 control-label">{{Type}}</label>
+									<div class="col-sm-6">
+										<select type="text" disabled class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="type" >
+											<option value="station">{{Station}}</option>
+											<option value="module_ext">{{Module extérieur}}</option>
+											<option value="module_int">{{Module intérieur}}</option>
+											<option value="module_rain">{{Module pluie}}</option>
+											<option value="module_wind">{{Anémomètre}}</option>
+										</select>
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-sm-4 control-label">{{Firmware}}</label>
+									<div class="col-sm-6">
+										<span class="eqLogicAttr label label-info" style="font-size:1em;" data-l1key="configuration" data-l2key="firmware"></span>
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-sm-4 control-label">{{Réception réseaux}}</label>
+									<div class="col-sm-6">
+										<span class="label label-info" style="font-size:1em;">
+											<span class="eqLogicAttr" data-l1key="configuration" data-l2key="wifi_status"></span>
+											<span class="eqLogicAttr" data-l1key="configuration" data-l2key="rf_status"></span>
+										</span>
+									</div>
+								</div>
+								<div class="form-group" id="battery_net_weather">
+									<label class="col-sm-4 control-label">{{Batterie}}</label>
+									<div class="col-sm-6">
+										<span class="label label-info" style="font-size:1em;">
+											<span class="eqLogicAttr" data-l1key="configuration" data-l2key="batteryStatus"></span> %
+										</span>
+									</div>
+								</div>
+							</fieldset>
+						</form>
+					</div>
+					<div class="col-sm-6">
+						<center>
+							<img src="' . $plugin->getPathImgIcon() . '" id="img_netatmoModel" style="height : 300px;margin-top : 60px" />
+						</center>
+					</div>
+				</div>
+			</div>
 			<div role="tabpanel" class="tab-pane" id="commandtab">
-<legend><i class="fa fa-list-alt"></i>  {{Météo Netatmo}}</legend>
-<table id="table_cmd" class="table table-bordered table-condensed">
-  <thead>
-    <tr>
-      <th>{{Nom}}</th><th>{{Option}}</th><th>{{Action}}</th>
-    </tr>
-  </thead>
-  <tbody>
-  </tbody>
-</table>
-</div>
-</div>
-</div>
+				<legend><i class="fa fa-list-alt"></i>  {{Météo Netatmo}}</legend>
+				<table id="table_cmd" class="table table-bordered table-condensed">
+					<thead>
+						<tr>
+							<th>{{Nom}}</th><th>{{Option}}</th><th>{{Action}}</th>
+						</tr>
+					</thead>
+					<tbody>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php include_file('desktop', 'netatmoWeather', 'js', 'netatmoWeather');?>
