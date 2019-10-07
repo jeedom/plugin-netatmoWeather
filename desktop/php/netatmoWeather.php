07463d3c0d40487837d9b8dd9f0a55552383dbd2
Loïc <loic@jeedom.com>
2019-10-07 19:39:43 +0200

    fix

--- desktop/php/netatmoWeather.php
+++ desktop/php/netatmoWeather.php
@@ -40,7 +40,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 						echo '<img src="' . $plugin->getPathImgIcon() . '" />';
 					}
 					echo '<br/>';
-					echo '<span>' . $eqLogic->getHumanName(true, true) . '</span>';
+					echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
 					echo '</div>';
 				}
 				?>
